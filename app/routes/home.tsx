d9191f4ea81a77c3fe657fe86ae22d5623fb8121
Luffy-lulu <146710770+LuluH19@users.noreply.github.com>
2025-07-23 02:27:24 +0200

    feat: Refactor home page and remove welcome component
    - Updated home page to include a Navbar and a section for tracking resumes.
    - Removed the Welcome component and its associated assets (logo-dark.svg, logo-light.svg).
    - Introduced ResumeCard component to display individual resumes with feedback scores.
    - Added ScoreCircle component to visually represent feedback scores.
    - Created a constant resumes array to hold sample resume data for display.
    - Updated type definitions to include necessary fields for Resume and Feedback interfaces.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Welcome to Resumind!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+  <Navbar />
+
+
+   <section className="main-section">
+    <div className="page-header">
+     <h1>Track your Application & Resumes Rating</h1>
+     <h2>Review your submissions and get feedback</h2>
+   </div>
+
+  {resumes.length > 0 && (
+
+  <div className="resumes-section">
+    {resumes.map((resume) => (
+      <ResumeCard key={resume.id} resume={resume} />
+    ))}
+  </div>
+  )}
+  </section>
+  </main>;
 }
